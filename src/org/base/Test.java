c61631dc73bd5e10893945285f2edf3ce7d0c898
ReeganLennox <apple.reegan@gmail.com>
2023-07-31 13:46:32 +0530

    message

--- src/org/base/Test.java
+++ src/org/base/Test.java
@@ -8,18 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test {
 	
 	public static void main(String[] args) {
-		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Reegan\\eclipse-workspace\\Selenium\\driver\\chromedriver.exe");
-		
 		WebDriver driver = new ChromeDriver();
-		
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
 		WebElement txtusername = driver.findElement(By.id("email"));
 		txtusername.sendKeys("dbhss.reegan@gmail.com");
 		WebElement txtpassword= driver.findElement(By.id("pass"));
 		txtpassword.sendKeys("Reeganjava1!");
-		
 		WebElement btnLogin= driver.findElement(By.xpath("//button[@value='login']"));
 		btnLogin.click();
 		
