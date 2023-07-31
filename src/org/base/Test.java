dd875f1d18b9b97e44fb3d32205af6de0ac990c7
ReeganLennox <apple.reegan@gmail.com>
2023-07-31 12:54:01 +0530

    message

--- src/org/base/Test.java
+++ src/org/base/Test.java
@@ -1,6 +1,8 @@
 package org.base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
@@ -13,6 +15,15 @@ public class Test {
 		
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
+		
+		WebElement txtusername = driver.findElement(By.id("email"));
+		txtusername.sendKeys("dbhss.reegan@gmail.com");
+		
+		WebElement txtpassword= driver.findElement(By.id("pass"));
+		txtpassword.sendKeys("Reeganjava1!");
+		
+		WebElement btnLogin= driver.findElement(By.xpath("//button[@value='login']"));
+		btnLogin.click();
 	}
 
 }
